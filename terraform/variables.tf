752203bd98ed2ac72beba6253966882d852dcf0e
Zac Law <zacl@getport.io>
2025-10-01 15:50:44 +1000

    remove vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,15 @@
-variable "registry_url" {
-  description = "The URL of the container registry."
-  type        = string
-}
+# variable "registry_url" {
+#   description = "The URL of the container registry."
+#   type        = string
+# }
 
-variable "registry_username" {
-  description = "The username for the container registry."
-  type        = string
-}
+# variable "registry_username" {
+#   description = "The username for the container registry."
+#   type        = string
+# }
 
-variable "registry_password" {
-  description = "The password for the container registry."
-  type        = string
-  sensitive   = true
-}
+# variable "registry_password" {
+#   description = "The password for the container registry."
+#   type        = string
+#   sensitive   = true
+# }
